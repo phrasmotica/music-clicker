795d9bf3b73539f1333bbc998076388a9cfcf764
Julian Lawrance <julian.lawrance@gmail.com>
2024-10-19 18:43:15 +0100

    Create UI for a product that can be made, and more copies of it bought

---
+++ scripts/product.gd
@@ -0,0 +1,38 @@
+@tool
+class_name Product extends PanelContainer
+
+@export_range(1.0, 10.0)
+var base_time_seconds := 3.0
+
+@export
+var is_automated := false
+
+@onready
+var progress_bar: ProgressBar = %ProgressBar
+
+var _is_making := false
+
+func _ready():
+	reset_progress()
+
+func _process(delta: float) -> void:
+	if _is_making:
+		progress_bar.value += 100 * delta / base_time_seconds
+
+		if progress_bar.value >= 100:
+			reset_progress()
+			_is_making = false
+
+func make() -> void:
+	reset_progress()
+	_is_making = true
+
+func reset_progress() -> void:
+	progress_bar.value = 0
+
+func _on_buy_button_pressed() -> void:
+	print("Bought a product!")
+
+func _on_make_button_pressed() -> void:
+	print("Making a product...")
+	make()
