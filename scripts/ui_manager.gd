22d4b213a39aa3750e5aec8085b886c4c9ce7895
Julian L <julian.lawrance@gmail.com>
2024-10-26 14:56:40 +0100

    Only resize window when UI manager is ready in play mode

--- scripts/ui_manager.gd
+++ scripts/ui_manager.gd
@@ -7,10 +7,11 @@ func _on_score_manager_score_changed(score: int) -> void:
 	score_label.text = "£" + str(score)
 
 func _ready():
-	var screen_size := DisplayServer.screen_get_size()
-	var small_window_size := screen_size * 2 / 3
+	if not Engine.is_editor_hint():
+		var screen_size := DisplayServer.screen_get_size()
+		var small_window_size := screen_size * 2 / 3
 
-	var window := get_window()
+		var window := get_window()
 
-	window.size = small_window_size
-	window.move_to_center()
+		window.size = small_window_size
+		window.move_to_center()
