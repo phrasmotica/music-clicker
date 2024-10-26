d7f224ccc6c101c4caeca14de0531e6bd7efa1b0
Julian L <julian.lawrance@gmail.com>
2024-10-26 13:51:03 +0100

    Resize game window to 66% of screen size when UI manager script is ready

--- scripts/ui_manager.gd
+++ scripts/ui_manager.gd
@@ -5,3 +5,12 @@ var score_label: Label = %ScoreLabel
 
 func _on_score_manager_score_changed(score: int) -> void:
 	score_label.text = "£" + str(score)
+
+func _ready():
+	var screen_size := DisplayServer.screen_get_size()
+	var small_window_size := screen_size * 2 / 3
+
+	var window := get_window()
+
+	window.size = small_window_size
+	window.move_to_center()
