d6af5c6250a9a87ac1abc8f6764effbd2c77207c
Julian L <julian.lawrance@gmail.com>
2025-04-28 21:11:14 +0100

    Don't try to move the game window if it's embedded in the Godot editor

--- scripts/ui_manager.gd
+++ scripts/ui_manager.gd
@@ -9,15 +9,17 @@ func _on_score_manager_score_changed(score: int) -> void:
 		score_label.text = "£" + str(score)
 
 func _ready():
-	if not Engine.is_editor_hint():
-		var screen_size := DisplayServer.screen_get_size()
-		var small_window_size := screen_size * 2 / 3
+	if Engine.is_editor_hint() or Engine.is_embedded_in_editor():
+		return
 
-		# display at 16:9 regardless of screen's aspect ratio
-		var small_window_height := small_window_size.x * 9.0 / 16
-		small_window_size.y = int(small_window_height)
+	var screen_size := DisplayServer.screen_get_size()
+	var small_window_size := screen_size * 2 / 3
 
-		var window := get_window()
+	# display at 16:9 regardless of screen's aspect ratio
+	var small_window_height := small_window_size.x * 9.0 / 16
+	small_window_size.y = int(small_window_height)
 
-		window.size = small_window_size
-		window.move_to_center()
+	var window := get_window()
+
+	window.size = small_window_size
+	window.move_to_center()
